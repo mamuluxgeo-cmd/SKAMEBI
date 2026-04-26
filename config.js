14dd8087d239a96bf4961e1dc28b8fae9115735f
mamuluxgeo-cmd <mamuluxgeo@gmail.com>
2026-04-26 18:38:21 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const SHEET_ID = "1SKtb8TwauWFtCHlP3QU8hw_YYuDAfMa798SysVAMVRM";
 
-const PRODUCTS =
+const PRODUCTS_CSV_URL =
   `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=0`;
 
-const CONFIG =
+const CONFIG_CSV_URL =
   `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=1117596975`;
